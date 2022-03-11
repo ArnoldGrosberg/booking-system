cb59ace6eae7edb860d132beda8ac102d1d25c9b
Arnold Grosberg <arnold.grosberg@khk.ee>
2022-03-11 17:51:27 +0200

    wip

--- api/index.js
+++ api/index.js
@@ -14,13 +14,31 @@ app.use(express.json()) // Populate req.body
 
 
 const widgets = [
-
-    { id: 1, name: "Cizzbor", price: 29.99 },
-
-    { id: 2, name: "Woowo", price: 26.99 },
-
-    { id: 3, name: "Crazlinger", price: 59.99 },
-
+/*
+    { id: 1, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 2, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 3, day: "Monday", start: "8:30", end:"9:00"  },
+    { id: 4, day: "Monday", start: "9:00", end:"9:30"  },
+    { id: 5, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 6, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 7, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 8, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 9, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 10, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 11, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 12, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 13, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 14, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 15, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 16, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 17, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 18, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 19, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 20, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 1, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 1, day: "Monday", start: "8:00", end:"8:30"  },
+    { id: 3, day: "Crazlinger", price: 59.99 },
+*/
 ]
 
 
