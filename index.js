0f9cbb0cfd5eeca53351afdfb0a5a0d823a42fb3
Arnold Grosberg <arnold.grosberg@khk.ee>
2022-03-18 18:09:09 +0200

    As a user I can book [Delivers #181538265]

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const times = [
     {id: 4, day: "2022-02-17", start: "9:00", end: "9:30", bookedBy: "Test2"}
 ]
 
+app.get('/times', (req, res) => {
+    res.send(times)
+})
+
 app.get('/times/available', (req, res) => {
     var timesAvailable = [];
     var i = 0;
@@ -23,6 +27,42 @@ app.get('/times/available', (req, res) => {
         i++;
     }
     res.send(timesAvailable)
+})
+
+app.get('/times/:id', (req, res) => {
+    if (typeof times[req.params.id - 1] === 'undefined') {
+        return res.status(404).send({error: "Time not found"})
+    }
+    res.send(times[req.params.id - 1])
+})
+
+app.patch('/times/:id', (req, res) => {
+
+    // Check that :id is a valid number
+    if ((Number.isInteger(req.params.id) && req.params.id > 0)) {
+        return res.status(400).send({error: 'Invalid id'})
+    }
+
+    // Check that time with given id exists
+    if(!times[req.params.id - 1]) {
+        return res.status(404).send({error: 'Time not found'})
+    }
+
+    // Check that name is valid
+    if(!/^\w{4,}/.test(req.body.name)){
+        return res.status(400).send({error: 'Invalid name'})
+    }
+
+    // Check that phone is valid
+    if(!/^\+?[1-9]\d{6,14}$/.test(req.body.phone)){
+        return res.status(400).send({error: 'Invalid phone'})
+    }
+
+    // Change name and phone for given id
+    times[req.params.id - 1]['bookedBy'] = req.body.name
+    times[req.params.id - 1]['phone'] = req.body.phone
+
+    res.status(200).end()
 })
 
 app.listen(8080, () => {
