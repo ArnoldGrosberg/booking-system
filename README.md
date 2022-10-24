5910ead4639f8e0eaeaec51ad6ca56e4cd33f320
ArnoldITA20 <arnold.grosberg@voco.ee>
2022-10-24 16:38:33 +0300

    Move to HTTPS

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# booking-system
+
+# Booking-system
+## Prerequisites
+Clone down this repository. You will need `node` and `npm` installed globally on your machine.  
+
+## HTTPS SSL certificate generation:
+### Creating an SSL Certificate
+To configure an SSL certificate, you can either use a public, trusted certificate or a self-signed certificate. 
+
+If you’re running the application in a production environment, always be sure to acquire and install a trusted certificate, not a self-signed certificate!
+
+### Creating self-signed certificate (for testing)
+1. First, generate a key file used for self-signed certificate generation with the command below. The command will create a private key as a file called key.pem.
+```
+openssl genrsa -out key.pem
+```
+
+2. Next, generate a certificate service request (CSR) with the command below. You’ll need a CSR to provide all of the input necessary to create the actual certificate.
+```
+openssl req -new -key key.pem -out csr.pem
+```
+
+3. Finally, generate your certificate by providing the private key created to sign it with the public key created in step two with an expiry date of 9,999 days. This command below will create a certificate called cert.pem.
+```
+openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem
+```
+
+4. Visit [https://localhost:4000](https://localhost:4000) and make your web browser trust your self-signed certificate by clicking advanced and then clicking the link to continue to the website.
+
+## Dependencies installation:
+```
+npm install
+```
+
+## Starting the back-end server
+```
+node ./index.js
+```
+
+## Running the app
+To Visit App, just open the index.html in your app folder.
--- index.js
+++ index.js
@@ -1,30 +1,47 @@
+const https = require('https');
+const fs = require('fs');
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = 8080
-var expressWs = require('express-ws')(app);
+
+let httpsServer = https
+    .createServer(
+        // Provide the private and public key to the server by reading each
+        // file's content with the readFileSync() method.
+        {
+            key: fs.readFileSync("key.pem"),
+            cert: fs.readFileSync("cert.pem"),
+        },
+        app
+    )
+
+    .listen(port, () => {
+        console.log("serever is runing at port " + port);
+    });
+var expressWs = require('express-ws')(app, httpsServer);
 app.use(cors())        // Avoid CORS errors in browsers
 app.use(express.json()) // Populate req.body
 
-app.ws('/', function(ws, req) {
-    ws.on('message', function(msg) {
-      expressWs.getWss().clients.forEach(client => client.send(msg));
+app.ws('/', function (ws, req) {
+    ws.on('message', function (msg) {
+        expressWs.getWss().clients.forEach(client => client.send(msg));
     });
-  });
+});
 
 let times = [
-    {id: 1, day: "2022-02-14", start: "8:00", end: "8:30", bookedBy: ""},
-    {id: 2, day: "2022-02-15", start: "8:00", end: "8:30", bookedBy: "Testing123"},
-    {id: 3, day: "2022-02-16", start: "8:30", end: "9:00", bookedBy: ""},
-    {id: 4, day: "2022-02-17", start: "9:00", end: "9:30", bookedBy: "Test2"}
+    { id: 1, day: "2022-02-14", start: "8:00", end: "8:30", bookedBy: "" },
+    { id: 2, day: "2022-02-15", start: "8:00", end: "8:30", bookedBy: "Testing123" },
+    { id: 3, day: "2022-02-16", start: "8:30", end: "9:00", bookedBy: "" },
+    { id: 4, day: "2022-02-17", start: "9:00", end: "9:30", bookedBy: "Test2" }
 ]
 const users = [
-    {id: 1, username: "Admin", password: "Password", isAdmin: true},
-    {id: 2, username: "User", password: "Password", isAdmin: false}
+    { id: 1, username: "Admin", password: "Password", isAdmin: true },
+    { id: 2, username: "User", password: "Password", isAdmin: false }
 ]
 
 let sessions = [
-    {id: 1, userId: 1}
+    { id: 1, userId: 1 }
 ]
 
 function isValidFutureDate(req) {
@@ -36,24 +53,24 @@ function isValidFutureDate(req) {
 function requireAdmin(req, res, next) {
     // Check that the sessionId is present
     if (!req.body.sessionId) {
-        return res.status(400).send({error: 'You have to login'})
+        return res.status(400).send({ error: 'You have to login' })
     }
 
     // Check that the sessionId is valid
     const sessionUser = sessions.find((session) => session.id === parseInt(req.body.sessionId));
     if (!sessionUser) {
-        return res.status(401).send({error: 'Invalid sessionId'})
+        return res.status(401).send({ error: 'Invalid sessionId' })
     }
 
     // Check that the sessionId in the sessions has user in it
     const user = users.findById(sessionUser.userId);
     if (!user) {
-        return res.status(400).send({error: 'SessionId does not have an user associated with it'})
+        return res.status(400).send({ error: 'SessionId does not have an user associated with it' })
     }
 
     // Check that the user is an admin
     if (!user.isAdmin) {
-        return res.status(400).send({error: 'Insufficient permissions'})
+        return res.status(400).send({ error: 'Insufficient permissions' })
     }
     next()
 }
@@ -79,48 +96,48 @@ app.get('/times', (req, res) => {
 app.patch('/times/edit/:id', requireAdmin, (req, res) => {
     // Check that :id is a valid number
     if ((Number.isInteger(req.params.id) && req.params.id > 0)) {
-        return res.status(400).send({error: 'Invalid id'})
+        return res.status(400).send({ error: 'Invalid id' })
     }
     let time = getTime(req);
     // Check that time with given id exists
     if (!time) {
-        return res.status(404).send({error: 'Time not found'})
+        return res.status(404).send({ error: 'Time not found' })
     }
 
     // Change name, day, start, end and phone for given id if provided
     if (req.body.name) {
         // Check that name is valid
         if (!/^\w{2,}/.test(req.body.name)) {
-            return res.status(400).send({error: 'Invalid name'})
+            return res.status(400).send({ error: 'Invalid name' })
         }
         time.bookedBy = req.body.name
     }
 
     // Check that start is valid
     if (!req.body.start || !/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.test(req.body.start)) {
-        return res.status(400).send({error: 'Invalid start'})
+        return res.status(400).send({ error: 'Invalid start' })
     }
     time.start = req.body.start
 
     // Check that day is valid
     if (!req.body.day || !isValidFutureDate(req)) {
-        return res.status(400).send({error: 'Invalid day'})
+        return res.status(400).send({ error: 'Invalid day' })
     }
     time.day = req.body.day
 
     // Check that end is valid
     if (!req.body.end || !/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.test(req.body.end)) {
-        return res.status(400).send({error: 'Invalid end'})
+        return res.status(400).send({ error: 'Invalid end' })
     }
     // Check that end is bigger than start
     if (req.body.end < req.body.start) {
-        return res.status(400).send({error: 'Invalid end'})
+        return res.status(400).send({ error: 'Invalid end' })
     }
     time.end = req.body.end
     if (req.body.phone) {
         // Check that phone is valid
         if (!req.body.phone || !/^\+?[1-9]\d{6,14}$/.test(req.body.phone)) {
-            return res.status(400).send({error: 'Invalid phone'})
+            return res.status(400).send({ error: 'Invalid phone' })
         }
         time.phone = req.body.phone
     }
@@ -130,45 +147,45 @@ app.patch('/times/edit/:id', requireAdmin, (req, res) => {
 
 app.post('/times', requireAdmin, (req, res) => {
     // Add name, day, start, end and phone if provided
-    let newTime = {id: 0, day: "", start: "", end: "", bookedBy: ""}
+    let newTime = { id: 0, day: "", start: "", end: "", bookedBy: "" }
     if (req.body.name) {
         // Check that name is valid
         if (!/^\w{4,}/.test(req.body.name)) {
-            return res.status(400).send({error: 'Invalid name'})
+            return res.status(400).send({ error: 'Invalid name' })
         }
         newTime.bookedBy = req.body.name
     }
 
     // Check that start is valid
     if (!req.body.start || !/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.test(req.body.start)) {
-        return res.status(400).send({error: 'Invalid start'})
+        return res.status(400).send({ error: 'Invalid start' })
     }
 
     newTime.start = req.body.start
 
     // Check that day is valid
     if (!req.body.day || !isValidFutureDate(req)) {
-        return res.status(400).send({error: 'Invalid day'})
+        return res.status(400).send({ error: 'Invalid day' })
     }
 
     newTime.day = req.body.day
 
     // Check that end is valid
     if (!req.body.end || !/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.test(req.body.end)) {
-        return res.status(400).send({error: 'Invalid end'})
+        return res.status(400).send({ error: 'Invalid end' })
     }
 
     newTime.end = req.body.end
 
     // Check that end is bigger than start
     if (req.body.end < req.body.start) {
-        return res.status(400).send({error: 'Invalid end'})
+        return res.status(400).send({ error: 'Invalid end' })
     }
 
     if (req.body.phone) {
         // Check that phone is valid
         if (!/^\+?[1-9]\d{6,14}$/.test(req.body.phone)) {
-            return res.status(400).send({error: 'Invalid phone'})
+            return res.status(400).send({ error: 'Invalid phone' })
         }
         newTime['phone'] = req.body.phone
     }
@@ -187,12 +204,12 @@ app.post('/times', requireAdmin, (req, res) => {
 app.delete('/times/:id', requireAdmin, (req, res) => {
     // Check that :id is a valid number
     if ((Number.isInteger(req.params.id) && req.params.id > 0)) {
-        return res.status(400).send({error: 'Invalid id'})
+        return res.status(400).send({ error: 'Invalid id' })
     }
 
     // Check that time with given id exists
     if (!times.findById(req.params.id)) {
-        return res.status(404).send({error: 'Time not found'})
+        return res.status(404).send({ error: 'Time not found' })
     }
     times = times.filter((time) => time.id !== parseInt(req.params.id));
     expressWs.getWss().clients.forEach(client => client.send(parseInt(req.params.id)));
@@ -217,7 +234,7 @@ app.get('/times/available', async (req, res) => {
 app.get('/times/:id', (req, res) => {
     let time = getTime(req);
     if (!time) {
-        return res.status(404).send({error: "Time not found"})
+        return res.status(404).send({ error: "Time not found" })
     }
     res.send(time)
 })
@@ -226,22 +243,22 @@ app.patch('/times/:id', (req, res) => {
 
     // Check that :id is a valid number
     if ((Number.isInteger(req.params.id) && req.params.id > 0)) {
-        return res.status(400).send({error: 'Invalid id'})
+        return res.status(400).send({ error: 'Invalid id' })
     }
     let time = getTime(req);
     // Check that time with given id exists
     if (!time) {
-        return res.status(404).send({error: 'Time not found'})
+        return res.status(404).send({ error: 'Time not found' })
     }
 
     // Check that name is valid
     if (!/^\w{4,}/.test(req.body.name)) {
-        return res.status(400).send({error: 'Invalid name'})
+        return res.status(400).send({ error: 'Invalid name' })
     }
 
     // Check that phone is valid
     if (!/^\+?[1-9]\d{6,14}$/.test(req.body.phone)) {
-        return res.status(400).send({error: 'Invalid phone'})
+        return res.status(400).send({ error: 'Invalid phone' })
     }
 
     // Change name and phone for given id
@@ -252,15 +269,15 @@ app.patch('/times/:id', (req, res) => {
 })
 app.post('/users', (req, res) => {
     if (!req.body.username || !req.body.password) {
-        return res.status(400).send({error: 'One or all params are missing'})
+        return res.status(400).send({ error: 'One or all params are missing' })
     }
 
     let user = users.findBy('username', req.body.username);
     if (user) {
-        return res.status(409).send({error: 'Conflict: The user already exists. '})
+        return res.status(409).send({ error: 'Conflict: The user already exists. ' })
     }
 
-    users.push({id: users.length + 1, username: req.body.username, password: req.body.password, isAdmin: false})
+    users.push({ id: users.length + 1, username: req.body.username, password: req.body.password, isAdmin: false })
 
     user = users.findById(users.length);
     let newSession = {
@@ -268,15 +285,15 @@ app.post('/users', (req, res) => {
         userId: user.id
     }
     sessions.push(newSession)
-    res.status(201).send({sessionId: sessions.length})
+    res.status(201).send({ sessionId: sessions.length })
 })
 app.post('/sessions', (req, res) => {
     if (!req.body.username || !req.body.password) {
-        return res.status(400).send({error: 'One or all params are missing'})
+        return res.status(400).send({ error: 'One or all params are missing' })
     }
     const user = users.find((user) => user.username === req.body.username && user.password === req.body.password);
     if (!user) {
-        return res.status(401).send({error: 'Unauthorized: username or password is incorrect'})
+        return res.status(401).send({ error: 'Unauthorized: username or password is incorrect' })
     }
     let newSession = {
         id: sessions.length + 1,
@@ -284,14 +301,10 @@ app.post('/sessions', (req, res) => {
     }
     sessions.push(newSession)
     res.status(201).send(
-        {sessionId: sessions.length}
+        { sessionId: sessions.length }
     )
 })
 app.delete('/sessions', (req, res) => {
     sessions = sessions.filter((session) => session.id === req.body.sessionId);
     res.status(200).end()
-})
-
-app.listen(8080, () => {
-    console.log(`API up at: http://localhost:${port}`)
 })
